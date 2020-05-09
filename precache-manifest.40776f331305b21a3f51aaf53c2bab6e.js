bd2760b0ead4ddd2e2a0cd2defea42bc72fc4a1f
Dariusz Pudlik <dariusz.pudlik@poleng.pl>
2020-05-09 12:14:34 +0200

    Updates

---
+++ precache-manifest.40776f331305b21a3f51aaf53c2bab6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb42b63b396553c7d4162dd8fa56090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e0d983af8c6f137515",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d112f72e3fe2c1f9edd",
+    "url": "/static/js/2.8ea28179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ea28179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e0d983af8c6f137515",
+    "url": "/static/js/main.f177d467.chunk.js"
+  },
+  {
+    "revision": "c4cf8389f2779a376068",
+    "url": "/static/js/runtime-main.656275f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
